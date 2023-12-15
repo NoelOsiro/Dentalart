a8e28d7c4a4178709806a581e353c2fc5d591b0c
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2023-12-15 19:32:24 +0000

    card patients

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Card from 'react-bootstrap/Card';
 import ProgressBar from 'react-bootstrap/ProgressBar';
+import { supabase } from '../../utils/supabase'; // Import your Supabase client
 
 interface CardData {
   type: 'patientsAdded' | 'progress' | 'recordsVerified';
@@ -8,23 +9,66 @@ interface CardData {
   total?: number;
 }
 
-const cardsData: CardData[] = [
+const initialCardData: CardData[] = [
   {
     type: 'patientsAdded',
-    value: 1200, // You can replace this with the actual number of patients added
-    total: 1700,
+    value: 0,
+    total: 7000,
   },
   {
     type: 'progress',
-    value: 75, // You can replace this with the actual progress percentage
+    value: 0,
   },
   {
     type: 'recordsVerified',
-    value: 950, // You can replace this with the actual number of records verified
+    value: 0,
   },
 ];
 
 function CardBorder() {
+  const [cardsData, setCardsData] = useState<CardData[]>(initialCardData);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // Fetch the total number of rows in the patients table
+        const { count, error } = await supabase
+          .from('patients')
+          .select('count', { count: 'exact' });
+
+        if (error) {
+          console.error('Error fetching data:', error);
+          return;
+        }
+
+        const patientsAdded = count ? count : 0;
+        const progress = (patientsAdded / 7000) * 100;
+        const recordsVerified = patientsAdded;
+
+        // Update the state with the fetched data
+        setCardsData([
+          {
+            type: 'patientsAdded',
+            value: patientsAdded,
+            total: 7000,
+          },
+          {
+            type: 'progress',
+            value: progress,
+          },
+          {
+            type: 'recordsVerified',
+            value: recordsVerified,
+          },
+        ]);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <>
       {cardsData.map((card, index) => (
@@ -57,19 +101,18 @@ function renderCardContent(card: CardData): React.ReactNode {
     case 'patientsAdded':
       return (
         <Card.Text>
-          <span className='text-primary' style={{ fontWeight: 'bold', fontSize:32 }}>{card.value} / {card.total}</span> patients added (
-          <span className='text-primary' style={{ fontWeight: 'bold', color: 'warning' }}>{((card.value / card.total!) * 100).toFixed(2)}%</span>)
+          <span className='text-primary' style={{ fontWeight: 'bold', fontSize: 32 }}>{card.value} / {card.total}</span> patients added
         </Card.Text>
       );
     case 'progress':
       return (
         <>
-          <ProgressBar now={card.value} label={<span className='text-warning'  style={{ fontWeight: 'bold',fontSize:32 }}>{`${card.value}%`}</span>} visuallyHidden />
-          <Card.Text>{<span className='text-warning' style={{ fontWeight: 'bold',fontSize:32 }}>{`${card.value}%`}</span>} completed</Card.Text>
+          <ProgressBar now={card.value} label={<span className='text-warning' style={{ fontWeight: 'bold', fontSize: 32 }}>{`${card.value}%`}</span>} visuallyHidden />
+          <Card.Text>{<span className='text-warning' style={{ fontWeight: 'bold', fontSize: 32 }}>{((card.value / card.total!) * 100).toFixed(1)}%</span>} completed</Card.Text>
         </>
       );
     case 'recordsVerified':
-      return <Card.Text><span className='text-success' style={{ fontWeight: 'bold',fontSize:32}}>{card.value}</span> records verified</Card.Text>;
+      return <Card.Text><span className='text-success' style={{ fontWeight: 'bold', fontSize: 32 }}>{card.value}</span> records verified</Card.Text>;
     default:
       return null;
   }
