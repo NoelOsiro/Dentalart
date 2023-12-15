15aa928ab08a3b47958a20fb333990cc250a15c5
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2023-12-15 19:02:55 +0000

    logout

--- src/components/navbar/NavbarLinks.tsx
+++ src/components/navbar/NavbarLinks.tsx
@@ -2,19 +2,36 @@
 import React from 'react';
 import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { useNavigate } from 'react-router-dom';
+import { supabase } from '../../utils/supabase';
 
-const NavbarLinks: React.FC = () => (
-  <Nav className="justify-content-end flex-grow-1 pe-3">
-    <Nav.Link href="/home">Home</Nav.Link>
-    <Nav.Link href="#action2">Check In</Nav.Link>
-    <Nav.Link href="#action2">Patients</Nav.Link>
-    <NavDropdown title="Account" id={`offcanvasNavbarDropdown-expand-false`}>
-      <NavDropdown.Item href="#action3">Settings</NavDropdown.Item>
-      <NavDropdown.Item href="#action4">Profile</NavDropdown.Item>
-      <NavDropdown.Divider />
-      <NavDropdown.Item href="#action5">Logout</NavDropdown.Item>
-    </NavDropdown>
-  </Nav>
-);
+const NavbarLinks: React.FC = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      await supabase.auth.signOut();
+      navigate('/login');
+    } catch (error) {
+      console.error('Error during logout:', error);
+    }
+  };
+
+  return (
+    <Nav className="justify-content-end flex-grow-1 pe-3">
+      <Nav.Link href="/home">Home</Nav.Link>
+      <Nav.Link href="#action2">Check In</Nav.Link>
+      <Nav.Link href="#action2">Patients</Nav.Link>
+      <NavDropdown title="Account" id={`offcanvasNavbarDropdown-expand-false`}>
+        <NavDropdown.Item href="#action3">Settings</NavDropdown.Item>
+        <NavDropdown.Item href="#action4">Profile</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#" onClick={handleLogout}>
+          Logout
+        </NavDropdown.Item>
+      </NavDropdown>
+    </Nav>
+  );
+};
 
 export default NavbarLinks;
